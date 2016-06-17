be60b607a8e17eaeb0747dc315f5ed1568d8c0c7
sr <skyrousse@gmail.com>
2016-06-17 11:24:49 -0700

    add script that collects user inputs and stores them into a newPizza object that also has an array of topings inside of it

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,33 @@
 //Business logic
+  function Pizza (size) {
+    this.pizzaSize = size;
+    this.pizzaToppings = [];
+  };
 
+  Pizza.prototype.getToppings = function() {
+    var topping =
+    this.pizzaToppings.push(topping);
+  };
+
+  Pizza.prototype.calculateCost = function () {
+    //Pizza sizes have base value
+    //each additional topping is $3
+  };
+
+  // .each(function() {
+  //   debugger;
+  //   pizzaToppings.push(toppings);
+  // });//end toppings function
 
 //User interface logic
+$(document).ready(function() {
+  $("form#order").submit(function(event) {
+    event.preventDefault();
+    debugger;
+    var inputtedSize = $("input[type='radio'][name='size']:checked").val();
+    var newPizza = new Pizza(inputtedSize);
+    $.each($("input[name='toppings']:checked"), function(){
+      newPizza.pizzaToppings.push($(this).val());
+    });
+  });//end submit function
+});//end doc ready function
