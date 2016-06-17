1244719188c29dfb01f748e4234ac7f5435939f8
sr <skyrousse@gmail.com>
2016-06-17 11:45:10 -0700

    add prototype to calculate the total cost of the pizza order and html container to output that cost

--- js/scripts.js
+++ js/scripts.js
@@ -4,30 +4,23 @@
     this.pizzaToppings = [];
   };
 
-  Pizza.prototype.getToppings = function() {
-    var topping =
-    this.pizzaToppings.push(topping);
-  };
-
   Pizza.prototype.calculateCost = function () {
-    //Pizza sizes have base value
-    //each additional topping is $3
+    debugger;
+    var toppingsTotal = this.pizzaToppings.length * 3;
+    var orderTotal = this.pizzaSize + toppingsTotal;
+    return orderTotal;
   };
-
-  // .each(function() {
-  //   debugger;
-  //   pizzaToppings.push(toppings);
-  // });//end toppings function
 
 //User interface logic
 $(document).ready(function() {
   $("form#order").submit(function(event) {
     event.preventDefault();
-    debugger;
-    var inputtedSize = $("input[type='radio'][name='size']:checked").val();
+    var inputtedSize = parseInt($("input[type='radio'][name='size']:checked").val());
     var newPizza = new Pizza(inputtedSize);
-    $.each($("input[name='toppings']:checked"), function(){
+    $.each($("input[name='toppings']:checked"), function() {
       newPizza.pizzaToppings.push($(this).val());
     });
+    var outputTotal = newPizza.calculateCost();
+    $(".outputTotal").text(outputTotal);
   });//end submit function
 });//end doc ready function
